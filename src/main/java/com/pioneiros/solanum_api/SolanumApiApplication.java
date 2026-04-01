67b2eb160275931ee4bd165f17a8a11e459505ec
Patrick Gougeon <patrickgougeoncouto@gmail.com>
2026-04-01 19:41:06 -0300

    Primeiro commit + docker-compose!

---
+++ src/main/java/com/pioneiros/solanum_api/SolanumApiApplication.java
@@ -0,0 +1,13 @@
+package com.pioneiros.solanum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolanumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolanumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pioneiros/solanum_api/SolanumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioneiros.solanum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolanumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
